202287d66ade69b5b7a784e37bd76049927739c9
artem <xaeshkayt@gmail.com.>
2026-07-05 16:27:11 +0300

    Add PostgreSQL integration test with Testcontainers

---
+++ src/test/java/career/flow/owoke/OwokeApplicationIntegrationTest.java
@@ -0,0 +1,34 @@
+package career.flow.owoke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@SpringBootTest
+@Testcontainers
+@ActiveProfiles("test")
+class OwokeApplicationIntegrationTest {
+
+    @Container
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16")
+            .withDatabaseName("owoke_test")
+            .withUsername("owoke")
+            .withPassword("owoke_test_password");
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
+
+    @Test
+    void contextLoadsWithPostgresAndLiquibase() {
+    }
+
+}
